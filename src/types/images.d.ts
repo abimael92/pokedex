6a6e4970057596397f1d263a35dcd4fdb1c347de
Abimael <abimael1992g@gmail.com>
2025-07-11 08:14:10 -0600

    chore: update image types

--- src/types/images.d.ts
+++ src/types/images.d.ts
@@ -1,9 +1,9 @@
-declare module '*.gif' {
-  const value: string;
-  export default value;
+declare module '*.svg' {
+	const content: string;
+	export default content;
 }
 
-declare module '*.svg' {
-  const value: string;
-  export default value;
+declare module '*.gif' {
+	const content: string;
+	export default content;
 }
