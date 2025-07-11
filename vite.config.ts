23b50cdda92201c3661f9f4aa27fd5ddb9d9b07c
Abimael <abimael1992g@gmail.com>
2025-07-11 08:46:50 -0600

    chore: update vite.config.ts alias settings

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,11 @@ export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
-			'@assets': path.resolve(__dirname, 'src/assets'),
-			components: path.resolve(__dirname, 'src/components'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@hooks': path.resolve(__dirname, './src/hooks'),
+			'@utils': path.resolve(__dirname, './src/utils'),
+			'@services': path.resolve(__dirname, './src/services'),
 		},
 	},
 });
