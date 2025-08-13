d4829bb8c45f4ce23ae8a7244448f7322ac82856
Abimael Garcia <abimael1992g@gmail.com>
2025-08-13 12:41:35 -0600

    chore: update vite.config.ts after manual undo

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
 	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/setupTests.ts',
-	},
+
 	resolve: {
 		alias: {
 			'@components': path.resolve(__dirname, './src/components'),
